08a4d1cec1cddbcc1d175a16500fc1b4c5f46164
EllisonWu <35>
2018-02-11 12:30:18 +0800

    quicksort_fast

--- QuickSort.py
+++ QuickSort.py
@@ -1,25 +1,53 @@
+a=[]
+for line in open('4.txt'):
+    a.append(int(line[:]))
+count=0
 
 
-def Sort(array,start,end):
-    mid = start
-    for i in range (start,end-1):
-        if array[i] < array[end-1]:
+#start
+def Sort_start(array,start,end):
+    global count
+    mid = start+1
+    count += (end-start-1)
+    for i in range (start,end):
+        if array[i] < array[start]:
             ex = array[mid]
             array[mid] = array[i]
             array[i] = ex
             mid += 1
-    ex2 = array[mid]
-    array[mid] = array[end-1]
-    array[end-1] = ex2
-    return mid
+    ex2 = array[mid-1]
+    array[mid-1] = array[start]
+    array[start]= ex2
+    return mid-1
+
+
 
 def QuickSort(array,start,end):
     if start < end-1:
-        separ = Sort(array,start,end)
+        tab={array[start]:start,array[end-1]:end-1,array[(end-start-1)//2+start]:(end-start-1)//2+start}
+        box=[array[start],array[end-1],array[(end-start-1)//2+start]]
+        box.remove(min(box))
+        box.remove(max(box))
+        comp=tab[box.pop(0)]
+        if comp == start:
+            separ = Sort_start(array,start,end)
+        elif comp == end:
+            ex2 = array[start]
+            array[start] = array[end-1]
+            array[end-1]= ex2
+            separ = Sort_start(array,start,end)
+
+        else:
+            ex2 = array[start]
+            array[start] = array[comp]
+            array[comp]= ex2
+            separ = Sort_start(array,start,end)
         QuickSort(array,start,separ)
         QuickSort(array,separ+1,end)
+        return
     else:
         pass
-a = [1,4,2,0,-1,6,54,3,7,12,4,6,58,9,234,43546,2,5,76,78,2,45,7,6,-32,-2,3-4,5,3]
+
 QuickSort(a,0,len(a))
-print(a)
+#print(a)
+print(a,count)
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-l=[]
-for line in open('as.txt'):
-    l.append(line[:-1])
-print(l[:2])
+array=[1,2,3,45,6,7,8,9,34,453,345,23,56,1234,12]
+
+start=0
+end=len(array)
+
+
+tab={array[start]:start,array[end-1]:end-1,array[(end-start-1)//2+start]:(end-start-1)//2+start}
+box=[array[start],array[end-1],array[(end-start-1)//2+start]]
+box.remove(min(box))
+box.remove(max(box))
+comp=tab[box.pop(0)]
+print(array,comp,start,end,tab)
